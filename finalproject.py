67bc4d1ccb5b304194cb8a58e5ba7ab24aa2a6f1
TheSpeedForce <agg.pawas@gmail.com>
2021-12-12 01:51:01 -0500

    working basic

--- finalproject.py
+++ finalproject.py
@@ -1,4 +1,3 @@
-from _typeshed import FileDescriptor
 from threading import Thread
 
 # Import Raspberry Pi GPIO library
@@ -32,7 +31,7 @@ wait_time = 2
 
 # Setting input sensors
 GPIO.setup(ldr_pin, GPIO.IN)
-GPIO.setup(button_pin, GPIO.IN)
+GPIO.setup(button_pin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 dht_sensor = DHT.DHT11
 
@@ -50,7 +49,7 @@ class RGB_LED(object):
         self.commands = {"on": GPIO.HIGH, "off": GPIO.LOW}
     def setup(self):
         for pin in self.pins.values():
-            GPIO.setup (pin, GPIO.out)
+            GPIO.setup (pin, GPIO.OUT)
     def output(self, pin, command):
         GPIO.output(self.pins[pin], self.commands[command])
 
@@ -79,20 +78,29 @@ class RGB_LED(object):
 #     def water_sensor(self):
 #         pass
     
-# def weather_reporter(object):
-#     def __init__(self, button_pin, led_pin):
-#         self.weather_sensors = weather_reporter(dht_pin, ldr_pin, water_pin)
-#         self.button_pin = button_pin
-#         self.rgb_led = RGB_LED(led_pin)
-#     def camera_module(self):
-#         while True:
-#             os.system("raspistill -o test.jpg")
-#             sleep(2)
-#     def led_reporting(self):
-#         pass
-#     def run(self):
-#         if self.running:
-#             pass
+class weather_reporter(object):
+    def __init__(self):
+        self.running = True
+    def terminate(self):
+        self.running = False
+    def log_reading(self):
+        while self.running:
+            humidity,temperature = DHT.read_retry(dht_sensor, dht_pin)
+            if humidity is not None and temperature is not None:
+                print (f'Humidity: {humidity}%, Temperature: {temperature}*C')
+            else:
+                print ("Reading failed.")
+            print (GPIO.input(ldr_pin))
+            print ("")
+            # try: 
+            #     print('Raw ADC Value: ', channel.value)
+            #     print('ADC Voltage: ' + str(channel.voltage) + 'V')
+            #     print ("")
+            # except:
+            #     print ("Something is wrong.")
+            
+            #os.system("raspistill -o test.jpg -n")
+            time.sleep(wait_time)
 
 
 # 1. everything is off, led is blue (standby)
@@ -105,13 +113,14 @@ class RGB_LED(object):
 # 8. repeat from 4
 # 9. shows error on screen, exit
 global running
-running = True
+running = False
+
 
 
 def switch():
     ON = True
     OFF = False
-    state = ON
+    state = OFF
     rgb_led = RGB_LED(rgb_red_pin, rgb_blue_pin, rgb_green_pin)
     rgb_led.output("blue", "on")
     while True:
@@ -123,10 +132,12 @@ def switch():
                 state = ON
             else:
                 state = OFF
+
         if state == ON:
             try:
-                global running
-                running = True
+                reporter  = weather_reporter()
+                reporterThread = Thread(target = reporter.log_reading)
+                reporterThread.start();
                 rgb_led.output("red", "off")
                 rgb_led.output("blue", "off")
                 rgb_led.output("green", "on")
@@ -136,8 +147,7 @@ def switch():
                 rgb_led.output("green", "off")
         else:
             try:
-                global running
-                running = False
+                reporter.terminate()
                 rgb_led.output("red", "off")
                 rgb_led.output("blue", "on")
                 rgb_led.output("green", "off")
@@ -146,31 +156,82 @@ def switch():
                 rgb_led.output("blue", "off")
                 rgb_led.output("green", "off")
            
-def log_reading():
-    global running
-    while running:
-        humidity,temperature = DHT.read_retry(dht_sensor, dht_pin)
-        if humidity is not None and temperature is not None:
-            print (f'Humidity: {humidity}%, Temperature: {temperature}*C')
-        else:
-            print ("Reading failed.")
-        print (GPIO.input(ldr_pin))
-        print ("")
-        # try: 
-        #     print('Raw ADC Value: ', channel.value)
-        #     print('ADC Voltage: ' + str(channel.voltage) + 'V')
-        #     print ("")
-        # except:
-        #     print ("Something is wrong.")
+
+#switch_thread = Thread (target = switch)
+
+#switch_thread.start()
+global off
+off = True
+def on(button_pin):
+    global off
+    if off:
+        print (1)
+        rgb_led.output("green", "on")
+        off = False
+        time.sleep(2)
+    else:
+        rgb_led.output("green", "off")
+        off = True
+        time.sleep(2)
         
-        os.system("raspistill -o test.jpg")
-        time.sleep(wait_time)
-switch_thread = Thread (target = switch)
-log_reading_thread = Thread (targer = log_reading)
-
-switch_thread.start()
-log_reading_thread.start()
+def off():
+    rgb_led("green", "off")
+    
+#GPIO.add_event_detect(button_pin, GPIO.RISING, callback = on)
 
 
+rgb_led = RGB_LED(rgb_red_pin, rgb_blue_pin, rgb_green_pin)
+rgb_led.output("blue", "on")
+ON = True
+OFF = False
+state = OFF
+    
+while True:
+    button_state = GPIO.input(button_pin)
+    print(button_state)
+    if button_state == 0:
+        time.sleep(1)
+        if state == OFF:
+            state = ON
+        else:
+            state = OFF
+
+    if state == ON:
+        try:
+            rgb_led.output("red", "off")
+            rgb_led.output("blue", "off")
+            rgb_led.output("green", "on")
+            humidity,temperature = DHT.read_retry(dht_sensor, dht_pin)
+            if humidity is not None and temperature is not None:
+                print (f'Humidity: {humidity}%, Temperature: {temperature}*C')
+            else:
+                print ("Reading failed.")
+            print (GPIO.input(ldr_pin))
+            print ("")
+            # try: 
+            #     print('Raw ADC Value: ', channel.value)
+            #     print('ADC Voltage: ' + str(channel.voltage) + 'V')
+            #     print ("")
+            # except:
+            #     print ("Something is wrong.")
+            
+            #os.system("raspistill -o test.jpg -n")
+            time.sleep(wait_time)
+
+            
+        except:
+            rgb_led.output("red", "on")
+            rgb_led.output("blue", "off")
+            rgb_led.output("green", "off")
+    else:
+        try:
+            rgb_led.output("red", "off")
+            rgb_led.output("blue", "on")
+            rgb_led.output("green", "off")
+        except:
+            rgb_led.output("red", "on")
+            rgb_led.output("blue", "off")
+            rgb_led.output("green", "off")
+       
 
 
